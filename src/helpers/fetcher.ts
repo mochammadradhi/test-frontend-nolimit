deb1bd9b2d841ddcd5bb656f7dde4d17529da574
mochammadradhi <m.radhiakbar@gmail.com>
2025-05-07 12:08:29 +0700

    solving eslint error

--- src/helpers/fetcher.ts
+++ src/helpers/fetcher.ts
@@ -15,14 +15,14 @@ interface ApiResponse<T = unknown> {
 
 const GlobalGet = async <T = unknown>(args: FetcherArgs): Promise<ApiResponse<T>> => {
     try {
-        const response: AxiosResponse<ApiResponse<T>> = await axios({ ...args, method: 'GET', timeout: 5000 }); // Updated timeout to 5000ms
+        const response: AxiosResponse<ApiResponse<T>> = await axios({ ...args, method: "GET", timeout: 5000 }); // Updated timeout to 5000ms
         return response.data;
     } catch (err: unknown) {
         if (axios.isAxiosError(err) && err.response) {
-            return {status:"error", message:err.response.data};
+            return { status: "error", message: String(err.response.data) }; 
         }
-        return {status:"error", message: 'Unknown error occurred, Please Try Again!' };
+        return { status: "error", message: "Unknown error occurred, Please Try Again!" };
     }
-}
+};
 
 export { GlobalGet };
